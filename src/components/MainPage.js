b04dee08cb251b9610672438eb7408351152b68d
msyon2 <suyon.korea@gmail.com>
2022-06-25 21:20:03 +0900

    additional react-router-dom links

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -63,10 +63,10 @@ const MainPage = () => {
         </div>
       </div>
       <div id="footer">
-        <a href="#">회사소개</a>
-        <a href="#">이용약관</a>
-        <a href="#">통신판매업:123-1234</a>
-        <a href="#">사업자등록번호:456-56-78951</a>
+        <Link to="/about">회사소개</Link>
+        <Link to="/policy">이용약관</Link>
+        <Link to="/sales">통신판매업:123-1234</Link>
+        <Link to="/license">사업자등록번호:456-56-78951</Link>
       </div>
     </div>
   );
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const ProductPage = () => {
   const { id } = useParams();
+  const Navigate = useNavigate();
   return (
     <div>
+      <button onClick={() => Navigate(-1)}>back</button>
       <h1>{id}번째 상품페이지</h1>
       <p>{id}번째 상품페이지 입니다</p>
     </div>
