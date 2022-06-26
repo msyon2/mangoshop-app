3bcbdafb9c397be4945811126573c0487abc0268
msyon2 <suyon.korea@gmail.com>
2022-06-26 13:33:19 +0900

    footer section relocated to App js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { ReactFragment } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import UploadPage from "./components/UploadPage";
 import ProductPage from "./components/ProductPage";
@@ -12,6 +12,12 @@ function App() {
         <Route path="/upload" element={<UploadPage></UploadPage>} />
         <Route path="/products/:id" element={<ProductPage></ProductPage>} />
       </Routes>
+      <div id="footer">
+        <Link to="/about">회사소개</Link>
+        <Link to="/policy">이용약관</Link>
+        <Link to="/sales">통신판매업:123-1234</Link>
+        <Link to="/license">사업자등록번호:456-56-78951</Link>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -62,12 +62,12 @@ const MainPage = () => {
           })}
         </div>
       </div>
-      <div id="footer">
+      {/* <div id="footer">
         <Link to="/about">회사소개</Link>
         <Link to="/policy">이용약관</Link>
         <Link to="/sales">통신판매업:123-1234</Link>
         <Link to="/license">사업자등록번호:456-56-78951</Link>
-      </div>
+      </div> */}
     </div>
   );
 };
